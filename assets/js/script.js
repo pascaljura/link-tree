8695f972e6f939c01d61b964dbc583ddc130d571
pascal.jura <boucnik.jiri@gmail.com>
2025-01-12 21:06:45 +0100

    přidání statistik

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+<script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-EFPEG1VD81"
+></script>;
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-EFPEG1VD81");
