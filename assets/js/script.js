6c70f28edb3b2eabfdf854983ae277b0e86fdef9
pascal.jura <boucnik.jiri@gmail.com>
2025-01-12 21:14:33 +0100

    oprava

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-<script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=G-EFPEG1VD81"
-></script>;
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
